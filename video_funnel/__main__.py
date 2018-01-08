ffd9cd94d0646c7067b3a706f7c22f6418960cf4
Chen Shuaimin <i@tulip.ink>
2018-01-08 16:41:54 +0800

    prettify output

--- video_funnel/__main__.py
+++ video_funnel/__main__.py
@@ -1,9 +1,10 @@
 import asyncio
 import re
+from argparse import ArgumentParser
 
 from aiohttp import web
-from argparse import ArgumentParser
-from .__init__ import routes
+
+from .__init__ import handler
 
 
 def convert_unit(s):
@@ -14,23 +15,23 @@ def convert_unit(s):
 
 def main():
     ap = ArgumentParser(description='Funnel -- Use multiple connections to request the video, then feed the combined data to the player.')
-    ap.add_argument('--url', '-u', help='the video url')
-    ap.add_argument('--block-size', '-b', type=convert_unit, default='8M',
+    ap.add_argument('url', metavar='URL', help='the video url')
+    ap.add_argument('--block-size', '-b', metavar='N', type=convert_unit, default='4M',
                     help='size of one block')
-    ap.add_argument('--piece-size', '-p', type=convert_unit, default='1M',
+    ap.add_argument('--piece-size', '-p', metavar='N', type=convert_unit, default='1M',
                     help='size of one piece')
-    ap.add_argument('--timeout', '-t', type=int, default=60,
+    ap.add_argument('--disable-bar', '-q', action='store_true',
+                    help="Don't show the progress bar for each block")
+    ap.add_argument('--timeout', '-t', type=int, metavar='N', default=60,
                     help='timeout in request')
-    ap.add_argument('--max-tries', '-r', type=int, default=10,
+    ap.add_argument('--max-tries', '-r', type=int, metavar='N', default=10,
                     help='Limit retries on network errors.')
     args = ap.parse_args()
 
-    app = web.Application(debug=True)
-    app.router.add_routes(routes)
+    app = web.Application()
+    app.router.add_get('/', handler)
     app['args'] = args
     app['session'] = None
-    #  import aiohttp_debugtoolbar
-    #  aiohttp_debugtoolbar.setup(app)
     try:
         web.run_app(app, loop=asyncio.get_event_loop())
     except KeyboardInterrupt:
